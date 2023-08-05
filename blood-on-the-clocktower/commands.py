9be6894dc85e5421ef81a224bd614ab20f301251
Dwayne Yuen <dwayneyuen@gmail.com>
2023-08-04 23:30:31 -0700

    feat: add status command

--- blood-on-the-clocktower/commands.py
+++ blood-on-the-clocktower/commands.py
@@ -1,9 +1,19 @@
+from typing import List
 from discord import Colour
 from discord.ext.commands.context import Context
+from prisma.models import Player
 
 from .bot import bot
+from .configurations import MINIMUM_PLAYERS
 from .prisma import prisma_client
 from .utils.message import send_message
+
+
+def queue_string(players: List[Player]) -> str:
+    if len(players) < MINIMUM_PLAYERS:
+        return f"Current queue (**{len(players)}**) (_{MINIMUM_PLAYERS} required_): {', '.join([f'**{player.display_name}**' for player in players])}"
+    else:
+        return f"Current queue ({len(players)}): {', '.join([f'**{player.display_name}**' for player in players])}"
 
 
 @bot.command()
@@ -11,20 +21,50 @@ async def add(ctx: Context):
     # TODO:
     # - Check if player is already queued
     # - Check if player is already in game
+    player = await prisma_client.player.find_unique(
+        where={
+            "discord_id": str(ctx.message.author.id),
+        }
+    )
+    queued_players = await prisma_client.player.find_many(where={"is_queued": True})
+    if player.is_queued:
+        await send_message(
+            ctx.channel,
+            embed_description=f"**{ctx.message.author.display_name}** is already in the queue!\n\n{queue_string(queued_players)}",
+            colour=Colour.red(),
+        )
+        return
+
     await prisma_client.player.update(
         where={
             "discord_id": str(ctx.message.author.id),
         },
         data={"is_queued": True},
     )
-    queued_players = await prisma_client.player.find_many(where={"is_queued": True})
-    await send_message(
-        ctx.channel,
-        embed_description=f"**{ctx.message.author.display_name}** added to queue\n\nCurrent queue ({len(queued_players)}): {', '.join([f'**{player.display_name}**' for player in queued_players])}",
-        colour=Colour.green(),
-    )
+    if len(queued_players) < MINIMUM_PLAYERS:
+        await send_message(
+            ctx.channel,
+            embed_description=f"**{ctx.message.author.display_name}** added to queue\n\n{queue_string(queued_players)}",
+            colour=Colour.green(),
+        )
+    else:
+        await send_message(
+            ctx.channel,
+            embed_description=f"**{ctx.message.author.display_name}** added to queue\n\n{queue_string(queued_players)}",
+            colour=Colour.green(),
+        )
 
 
 @bot.command()
 async def start(ctx: Context):
     pass
+
+
+@bot.command()
+async def status(ctx: Context):
+    queued_players = await prisma_client.player.find_many(where={"is_queued": True})
+    await send_message(
+        ctx.channel,
+        embed_description=queue_string(queued_players),
+        colour=Colour.green(),
+    )
---
+++ blood-on-the-clocktower/configurations.py
@@ -0,0 +1,31 @@
+from typing import NamedTuple
+
+MINIMUM_PLAYERS = 5
+
+
+class Configuration(NamedTuple):
+    players: int
+    minions: int
+    outsiders: int
+    townsfolk: int
+    imps: int
+
+
+configurations = [
+    None,  # 0
+    None,  # 1
+    None,  # 2
+    None,  # 3
+    None,  # 4
+    Configuration(players=5, minions=1, outsiders=0, townsfolk=3, imps=1),
+    Configuration(players=6, minions=1, outsiders=1, townsfolk=3, imps=1),
+    Configuration(players=7, minions=1, outsiders=0, townsfolk=5, imps=1),
+    Configuration(players=8, minions=1, outsiders=1, townsfolk=5, imps=1),
+    Configuration(players=9, minions=1, outsiders=2, townsfolk=5, imps=1),
+    Configuration(players=10, minions=2, outsiders=0, townsfolk=7, imps=1),
+    Configuration(players=11, minions=2, outsiders=1, townsfolk=7, imps=1),
+    Configuration(players=12, minions=2, outsiders=2, townsfolk=7, imps=1),
+    Configuration(players=13, minions=3, outsiders=0, townsfolk=9, imps=1),
+    Configuration(players=14, minions=3, outsiders=1, townsfolk=9, imps=1),
+    Configuration(players=15, minions=3, outsiders=2, townsfolk=9, imps=1),
+]
