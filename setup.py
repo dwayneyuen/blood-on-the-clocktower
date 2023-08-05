4e9694a947a1ac854964ab9aa1b4ba361b61910d
Dwayne Yuen <dwayneyuen@gmail.com>
2023-08-04 19:15:07 -0700

    feat: initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='blood-on-the-clocktower',
+    install_requires=requirements,
+    packages=find_packages(),
+    version='1.0',
+)
