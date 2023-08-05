184b0fe7b7777410161825c7b0167d082658680b
Dwayne Yuen <dwayneyuen@gmail.com>
2023-08-04 19:35:11 -0700

    feat: setup commit hooks

---
+++ blood-on-the-clocktower/bot.py
@@ -0,0 +1,19 @@
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+intents.members = True
+
+load_dotenv()
+COMMAND_PREFIX: str = os.getenv("COMMAND_PREFIX") or "!"
+
+
+bot = commands.Bot(
+    case_insensitive=True,
+    command_prefix=COMMAND_PREFIX,
+    help_command=commands.DefaultHelpCommand(verify_checks=False, dm_help=True),
+    intents=intents,
+)
