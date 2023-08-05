b7169e82c8a09592ae0fd485a77cd73de545f173
Dwayne Yuen <dwayneyuen@gmail.com>
2023-08-04 19:36:06 -0700

    lint: lint existing files

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='blood-on-the-clocktower',
+    name="blood-on-the-clocktower",
     install_requires=requirements,
     packages=find_packages(),
-    version='1.0',
+    version="1.0",
 )
